69804e80f8308d163d94f0fe2510986d75f324ae
Ki3mON <maciej.kmak@op.pl>
2024-12-29 17:06:07 +0100

    Added rectangle

---
+++ code/rectangle.py
@@ -0,0 +1,19 @@
+from typing import Tuple
+
+class Rectangle:
+    def __init__(self, xmin: float, ymin: float, xmax: float, ymax: float):
+        self.extreme = [[xmin, xmax], [ymin, ymax]]
+
+    def contains(self, point: Tuple[float, float]) -> bool:
+        return (
+            self.extreme[0][0] <= point[0] <= self.extreme[0][1]
+            and self.extreme[1][0] <= point[1] <= self.extreme[1][1]
+        )
+
+    def intersects(self, other: "Rectangle") -> bool:
+        return not (
+            self.extreme[0][1] < other.extreme[0][0]
+            or self.extreme[0][0] > other.extreme[0][1]
+            or self.extreme[1][1] < other.extreme[1][0]
+            or self.extreme[1][0] > other.extreme[1][1]
+        )
